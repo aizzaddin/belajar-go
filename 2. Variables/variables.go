1fe202ef04ea2a158ab18792efb10d89e884fc5a
Wildan Aizzaddin <39244724+aizzaddin@users.noreply.github.com>
2023-07-06 21:04:30 +0700

    :alembic: Update variable.go

--- 2. Variables/variables.go
+++ 2. Variables/variables.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
 )
 
@@ -19,4 +20,40 @@ func main() {
 
 	namaBelakang := "aizzaddin"
 	fmt.Println(strings.ToUpper(nama) + " " + strings.ToUpper(namaBelakang)) // WILDAN AIZZADDIN
+
+	// ARRAY
+	var namaArray = [5]string{"Ahmad", "Dila", "Wildan", "Caca", "Doni"}
+	fmt.Println(namaArray[1]) // Dila
+
+	namaArray[1] = "Indah"
+	fmt.Println(namaArray[1]) // Indah
+
+	// SLICES
+	var namaSlices = []string{"Ahmad", "Dila", "Wildan", "Caca", "Doni"}
+	fmt.Println(namaSlices) // [Ahmad Dila Wildan Caca Doni]
+
+	namaSlicesNew := append(namaSlices, "Galang", "Dani")
+	fmt.Println(namaSlicesNew) // [Ahmad Dila Wildan Caca Doni Galang Dani]
+
+	namaSlices1 := namaSlicesNew[:1]
+	fmt.Println(namaSlices1) // [Ahmad]
+
+	namaSlices1 = namaSlicesNew[2:3]
+	fmt.Println(namaSlices1) // [Wildan]
+
+	// MAPS
+	profil := make(map[string]string)
+	profil["nama"] = "Ahmad"
+	profil["umur"] = "20"
+
+	fmt.Println(profil["nama"]) // Ahmad
+	fmt.Println(profil["umur"]) // 20
+
+	delete(profil, "umur")
+	fmt.Println(profil["umur"]) // " "
+
+	// For Loops
+	for i := 0; i <= 10; i++ {
+		fmt.Println("Index ke-" + strconv.Itoa(i))
+	}
 }
